66ebfc6eb50a546d0491694f885f16e0019cc776
7red4 <abc401344@gmail.com>
2022-05-02 03:39:31 +0800

    add new section for twtter space description

--- src/page/HomePage.vue
+++ src/page/HomePage.vue
@@ -2,11 +2,7 @@
   <div class="h-7"></div>
   <div class="container mx-auto p-4">
     <div class="flex-wrap justify-space-between items-center sm:flex">
-      <div class="flex-1 my-3">
-        <img src="video_dance_odottemita.png" alt="" />
-      </div>
-      <div class="w-8"></div>
-      <div class="flex-1 my-3">
+      <div class="flex-1 flex justify-center my-3">
         《直播小幫手》是一隻提供 Vtuber
         直播通知、待機室建立、影片上傳、會限認證等服務的 Discord
         機器人。目前在超過 750 個伺服器中服務超過 300,000 位成員！
@@ -38,6 +34,20 @@
         <span class="text-red-400">Google</span>
         帳號，再進到伺服器中打上相關指令
         (詳見教學頁面)，便能瀏覽伺服器的會員限定頻道
+      </div>
+    </div>
+    <div class="h-7"></div>
+    <div class="flex-wrap justify-space-between items-center sm:flex">
+      <div class="flex-1 my-3">
+        <img src="video_dance_odottemita.png" alt="" />
+      </div>
+      <div class="w-8"></div>
+      <div class="flex-1 my-3">
+        除了
+        <span class="text-red-400">Google</span>
+        相關的服務以外，《直播小幫手》還支援
+        <span class="text-blue-400">Twitter</span>
+        Space (推特語音空間) 的提醒通知，讓你不再錯貴過任何突襲推特語音！
       </div>
     </div>
   </div>
