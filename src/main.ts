cd287835a864291ef6f6ca1c0a17fbd8fa910086
孤之界 <f20001012@Gmail.com>
2022-05-09 02:36:34 +0800

    del: tempToken

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './index.scss';
 
-import { randomNAString } from './lib/random';
-
 if (['/privacy', '/terms'].includes(location.pathname)) {
   //
 } else {
-  const tempToken: string =
-    sessionStorage.getItem('tempToken') || randomNAString(64);
-  sessionStorage.setItem('tempToken', tempToken);
-
   const app = createApp(App);
 
-  app.provide('tempToken', tempToken);
   app.provide(
     'googleClientId',
     '1001497098111-tbs3uidhom0r2p6gjaa8kqed4qp3oc61.apps.googleusercontent.com'
